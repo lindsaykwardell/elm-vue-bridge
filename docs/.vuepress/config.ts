1f0a348281c85d8c6ccf81774ed8a7bc3c0cdb0d
Lindasy Wardell <three060@gmail.com>
2021-11-14 07:05:55 -0800

    Add navbar and search

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -17,6 +17,20 @@ export default defineUserConfig<DefaultThemeOptions>({
       "/flags.md",
       "/ports.md",
     ],
+    navbar: [
+      {
+        text: "Getting Started",
+        link: "/getting-started",
+      },
+      {
+        text: "Elm Guide",
+        link: "https://guide.elm-lang.org/",
+      },
+      {
+        text: "Github Repo",
+        link: "https://github.com/lindsaykwardell/elm-vue-bridge",
+      },
+    ],
   },
 
   bundler: "@vuepress/bundler-vite",
@@ -25,4 +39,6 @@ export default defineUserConfig<DefaultThemeOptions>({
       plugins: [elmPlugin()],
     },
   },
+
+  plugins: [["@vuepress/plugin-search"]],
 });
