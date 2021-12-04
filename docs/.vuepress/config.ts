e8ddec4ed33b1124d5b21d722c72cea15a8d79fd
Lindsay Wardell <three060@gmail.com>
2021-12-04 15:59:49 -0800

    Add meta tags

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,19 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: "en-US",
   title: "elm-vue-bridge",
   description: "Render Elm modules in a Vue app",
-  head: [["link", { rel: "icon", href: "/images/elm-vue-bridge.webp" }]],
+  head: [
+    ["link", { rel: "icon", href: "/images/elm-vue-bridge.webp" }],
+    ["meta", { name: "og:type", content: "website" }],
+    // ["meta", { name: "og:url", content: "https://elm-vue-bridge.lindsaykwardell.com" }],
+    // ["meta", { name: "og:title", content: "elm-vue-bridge" }],
+    // ["meta", { name: "og:description", content: "Render Elm modules in a Vue app" }],
+    ["meta", { name: "og:image", content: "https://elm-vue-bridge.lindsaykwardell.com/images/elm-vue-bridge.webp" }],
+    ["meta", { name: "twitter:card", content: "summary_large_image" }],
+    // ["meta", { name: "twitter:url", content: "https://elm-vue-bridge.lindsaykwardell.com" }],
+    // ["meta", { name: "twitter:title", content: "elm-vue-bridge" }],
+    // ["meta", { name: "twitter:description", content: "Render Elm modules in a Vue app" }],
+    ["meta", { name: "twitter:image", content: "https://elm-vue-bridge.lindsaykwardell.com/images/elm-vue-bridge.webp" }],
+],
 
   themeConfig: {
     logo: "/images/elm-vue-bridge.webp",
